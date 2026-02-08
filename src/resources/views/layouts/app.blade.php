ee8dd173581bd2177dffb293ae82d444b7b07822
schec285 <mmmuni9511@gmail.com>
2026-02-08 19:19:48 +0900

    feat: 共通Viewのひな形作成

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'ラーメンブログ')</title>
+    <link rel="stylesheet" href="{{ asset('css/destyle.min.css') }}"
+    @vite('resources/js/app.js')
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="stylesheet"  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..400,0..1" />
+</head>
+<body>
+    @include('partials.header')
+
+    <main>
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
+</body>
+</html>
