a70cdd7e26890e567db96dfaea3b86a2a4d3da52
schec285 <mmmuni9511@gmail.com>
2026-02-19 23:18:05 +0900

    投稿画面をモックレベルの仮実装

--- src/resources/views/partials/header.blade.php
+++ src/resources/views/partials/header.blade.php
@@ -9,7 +9,9 @@
             @if(session('is_logged_in'))
                 <nav class="header__nav">
                     <ul class="header__nav-list">
+                        @if(!isset($hidePostBtn) || !$hidePostBtn)
                             <a href="{{ route('blogs.create') }}" class="header__create-btn btn">投稿</a>
+                        @endif
                         <form method="POST" action="{{ route('auth.logout') }}" class="header__nav-item">
                             @csrf
                             <button type="submit" class="header__nav-link btn">ログアウト</button>
