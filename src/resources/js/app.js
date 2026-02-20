7df24bad28b7649d6140f878c596e513418a6435
schec285 <mmmuni9511@gmail.com>
2026-02-21 00:21:56 +0900

    feat: jsをとりあえず書いた

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -4,8 +4,12 @@ import '../css/style.css';
 import '../css/github-markdown-light.css';
 
 import { loadMore } from './modules/loadMore';
+import { initScoreComponent } from './features/blogs/score';
+import { initThumbnailUpload } from './features/blogs/thumbnailUpload';
 
 loadMore();
+initScoreComponent();
+initThumbnailUpload();
 
 document.addEventListener('click', (e) => {
     const el = e.target.closest('[data-action]');
