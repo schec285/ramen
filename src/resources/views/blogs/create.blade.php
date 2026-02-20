3eac5d5ada7af0f96b81f3982651a7e5c46dc670
schec285 <mmmuni9511@gmail.com>
2026-02-20 23:33:11 +0900

    feat: 都道府県を取得してセレクトボックス生成

--- src/resources/views/blogs/create.blade.php
+++ src/resources/views/blogs/create.blade.php
@@ -29,8 +29,9 @@
                             </div>
                             <label class="blog-post__label" for="prefecture">都道府県<span class="required">*</span></label>
                             <select id="prefecture" class="blog-post__prefecture" required>
-                                <option value="">選択してください</option>
-                                <option value="1">東京都</option>
+                                @foreach($prefectures as $id => $name)
+                                    <option value="{{ $id }}">{{ $name }}</option>
+                                @endforeach
                             </select>
                             <label class="blog-post__label" for="city">市町村</label>
                             <input type="text" id="city" class="blog-post__input-text" placeholder="市町村を入力">
@@ -38,9 +39,11 @@
                             <input type="text" id="town" class="blog-post__input-text" placeholder="住所を入力">
                         </fieldset>
                     </div>
+<!--
                     <figure class="blog-post__map">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.4483700943483!2d139.79821637623232!3d35.715190028107024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188ec3b2f4da23%3A0xbe7d4695cdc73e01!2z44KJ44O844KB44KT5byB5oW2IOa1heiNieacrOW6lw!5e0!3m2!1sja!2sjp!4v1767522502164!5m2!1sja!2sjp" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </figure>
+-->
                     <div class="blog-post__upload">
                         <div class="blog-post__upload-area">
                             <label class="blog-post__upload-btn btn" for="thumbnail">
