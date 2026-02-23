ff6eb6b0463f06e3afadcade23d5239e1689db16
schec285 <mmmuni9511@gmail.com>
2026-02-23 12:36:12 +0900

    feat: ログイン仮実装 (#34)
    ## 対応内容
    
    - ルーティング設定
    - 画面
    - セッションを用いた仮認可
    
    ## 未対応
    
    - 認証
    - 認可
    
    close #33

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BlogController::class, 'index'])->name('blogs.index');
 Route::get('/load-more', [BlogController::class, 'loadMore'])->name('blogs.loadMore'); // ブログの追加読み込み
 
 Route::get('/blog/{blog}', [BlogController::class, 'show'])->name('blogs.show'); // ブログ詳細画面
+
+Route::get('/auth/login', [LoginController::class, 'login'])->name('auth.login'); // ログイン画面
+Route::post('/auth/login', [LoginController::class, 'authenticate']); // ログイン処理
+Route::post('/auth/logout', [LoginController::class, 'logout'])->name('auth.logout'); // ログアウト処理
